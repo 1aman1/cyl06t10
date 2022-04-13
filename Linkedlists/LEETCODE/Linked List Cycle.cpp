0a004a76bf5521176478491946b2cb13ac33fad7
1aman1 <amangupta@outlook.com>
2022-04-13 15:51:09 +0530

    slow and fast ptrs

--- Linkedlists/LEETCODE/Linked List Cycle.cpp
+++ Linkedlists/LEETCODE/Linked List Cycle.cpp
@@ -6,29 +6,33 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycle(ListNode *head)
+    {
         // set two pointers in list, singleStepPtr, DoubleStepPtr
         // they will collide before pointing to null, if the list is circular
         // otherwise, loop will brreak.
-        
+
         // incase head empty
-        if ( head == nullptr || head->next == nullptr ) return false;
+        if (head == nullptr || head->next == nullptr)
+            return false;
 
         ListNode *singleStepPtr, *doubleStepPtr;
-            
+
         singleStepPtr = doubleStepPtr = head;
-        
-        while ( doubleStepPtr && doubleStepPtr->next && doubleStepPtr->next->next ) {
+
+        while (doubleStepPtr && doubleStepPtr->next && doubleStepPtr->next->next)
+        {
             singleStepPtr = singleStepPtr->next;
             doubleStepPtr = doubleStepPtr->next->next;
 
             // they will collide before pointing to null, if the list is circular
-            if ( singleStepPtr == doubleStepPtr )
+            if (singleStepPtr == doubleStepPtr)
                 return true;
         }
-        
+
         return false;
     }
 };
