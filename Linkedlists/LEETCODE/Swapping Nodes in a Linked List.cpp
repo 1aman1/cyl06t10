9e03752a56266ed37e9321b78645ab4d1fe49102
1aman1 <amangupta@outlook.com>
2022-04-13 15:57:12 +0530

    not working

--- Linkedlists/LEETCODE/Swapping Nodes in a Linked List.cpp
+++ Linkedlists/LEETCODE/Swapping Nodes in a Linked List.cpp
@@ -1,17 +1,21 @@
 
 // Definition for singly-linked list.
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
- };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 //*/
-class Solution {
+class Solution
+{
 public:
     // ListNode* swapTwo
-    ListNode* swapNodes(ListNode* head, int k) {
+    ListNode *swapNodes(ListNode *head, int k)
+    {
 
-        if ( !head || ( head && !head->next ) ) {
+        if (!head || (head && !head->next))
+        {
             return head;
         }
 
@@ -19,20 +23,18 @@ public:
         traverse list, find ith node
         maintain k_front_prePtr and k_front_currPtr for ith Node
         first traversal should give length also
-        2nd traversal, 
+        2nd traversal,
         k_back_prePtr and k_back_currPtr for size -ith node
 
         interweave the list for getting swapped list.
         */
 
-       ListNode* traverse = head;
-       while ( traverse ) {
-           
-           traverse = traverse->next;
-           ++length;
-       }
+        ListNode *traverse = head;
+        while (traverse)
+        {
 
-       
-
+            traverse = traverse->next;
+            ++length;
+        }
     }
 };
