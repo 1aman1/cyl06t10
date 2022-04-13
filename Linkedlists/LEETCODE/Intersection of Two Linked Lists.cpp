78368e771e3489c855095da19c7d338efe51b27f
1aman1 <amangupta@outlook.com>
2022-04-13 15:50:50 +0530

    use length difference

--- Linkedlists/LEETCODE/Intersection of Two Linked Lists.cpp
+++ Linkedlists/LEETCODE/Intersection of Two Linked Lists.cpp
@@ -1,60 +1,76 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
         /*
         two list heads are given,
         1. we will gather length of the two different lists by keeping one counter
-        prepare two pointers A,B so that we can move either of them based on the 
+        prepare two pointers A,B so that we can move either of them based on the
         length difference
-        traverse two lists at same time, 
+        traverse two lists at same time,
         match their next pointers and break the loop if they same/ intersecting
         */
 
-       if ( !headA || !headB ) return nullptr;
+        if (!headA || !headB)
+            return nullptr;
 
-       // 1.
-       ListNode *traverseA = nullptr, *traverseB = nullptr;
-       int lengthA, lengthB, diff;
-       
-       traverseA = headA;
-       traverseB = headB;
-       lengthA = lengthB = 0;
+        // 1.
+        ListNode *traverseA = nullptr, *traverseB = nullptr;
+        int lengthA, lengthB, diff;
 
-       while ( traverseA ) { ++lengthA; traverseA = traverseA->next; }
-       while ( traverseB ) { ++lengthB; traverseB = traverseB->next; }
+        traverseA = headA;
+        traverseB = headB;
+        lengthA = lengthB = 0;
 
-       ( lengthA < lengthB ? diff = lengthB - lengthA : diff = lengthA - lengthB ) ;
+        while (traverseA)
+        {
+            ++lengthA;
+            traverseA = traverseA->next;
+        }
+        while (traverseB)
+        {
+            ++lengthB;
+            traverseB = traverseB->next;
+        }
 
-       // move the pointer of longer list
+        (lengthA < lengthB ? diff = lengthB - lengthA : diff = lengthA - lengthB);
 
-       traverseA = headA;
-       traverseB = headB;
+        // move the pointer of longer list
 
-       if ( lengthA > lengthB ) {
-           
-           while ( diff ) {
-               traverseA = traverseA->next;
-               --diff;
-           }
-       }
-       else {
-           
-           while ( diff ) {
-               traverseB = traverseB->next;
-               --diff;
-           }
-       }
+        traverseA = headA;
+        traverseB = headB;
 
-       // return the node when inntersection found
+        if (lengthA > lengthB)
+        {
 
-       while ( traverseA && traverseB ) {
-           if ( traverseA == traverseB ) {
-               return traverseA;               
-           }           
-           traverseA = traverseA->next;
-           traverseB = traverseB->next;
-           
-       }
-       return nullptr;
+            while (diff)
+            {
+                traverseA = traverseA->next;
+                --diff;
+            }
+        }
+        else
+        {
+
+            while (diff)
+            {
+                traverseB = traverseB->next;
+                --diff;
+            }
+        }
+
+        // return the node when inntersection found
+
+        while (traverseA && traverseB)
+        {
+            if (traverseA == traverseB)
+            {
+                return traverseA;
+            }
+            traverseA = traverseA->next;
+            traverseB = traverseB->next;
+        }
+        return nullptr;
     }
 };
