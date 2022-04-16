2c6e20b3d4f1dc261c065646049b0c591540409e
1aman1 <amangupta@outlook.com>
2022-04-16 19:48:44 +0530

    slow and fast coincide

--- Linkedlists/LEETCODE/Linked List Cycle II.cpp
+++ Linkedlists/LEETCODE/Linked List Cycle II.cpp
@@ -6,41 +6,47 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode *detectCycle(ListNode *head) {
+    ListNode *detectCycle(ListNode *head)
+    {
         // set two pointers in list, singleStepPtr, DoubleStepPtr
         // they will collide before pointing to null, if the list is circular
         // otherwise, loop will brreak.
-        
+
         // incase head empty
-        if ( head == nullptr || head->next == nullptr )  {
+        if (head == nullptr || head->next == nullptr)
+        {
             return nullptr;
         }
 
         ListNode *singleStepPtr, *doubleStepPtr;
-            
+
         singleStepPtr = doubleStepPtr = head;
-        
-        while ( doubleStepPtr && doubleStepPtr->next && doubleStepPtr->next->next ) {
+
+        while (doubleStepPtr && doubleStepPtr->next && doubleStepPtr->next->next)
+        {
 
             singleStepPtr = singleStepPtr->next;
             doubleStepPtr = doubleStepPtr->next->next;
 
             // they will collide before pointing to null, if the list is circular
-            if ( singleStepPtr == doubleStepPtr ) {
+            if (singleStepPtr == doubleStepPtr)
+            {
 
-                // start another single step pointer from head 
+                // start another single step pointer from head
                 ListNode *PtrFromHead = head;
 
-                while ( singleStepPtr != PtrFromHead ) {
+                while (singleStepPtr != PtrFromHead)
+                {
                     singleStepPtr = singleStepPtr->next;
                     PtrFromHead = PtrFromHead->next;
                 }
-                
-            return singleStepPtr;
+
+                return singleStepPtr;
             }
         }
-    return nullptr;
+        return nullptr;
     }
 };
