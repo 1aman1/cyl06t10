2ffc72d98b86928a84fc4c38c1643b20fc5d86a6
1aman1 <amangupta@outlook.com>
2022-04-16 19:51:51 +0530

    standard implementation

--- Linkedlists/LEETCODE/Remove Linked List Elements.cpp
+++ Linkedlists/LEETCODE/Remove Linked List Elements.cpp
@@ -8,24 +8,27 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
+    ListNode *addAtTail(int val, ListNode *head)
+    {
+        ListNode *newNode = new ListNode(val);
 
-    ListNode* addAtTail(int val, ListNode* head) {
-        ListNode* newNode = new ListNode(val);
-
-        // if list is empty 
-        if ( !head ) {
+        // if list is empty
+        if (!head)
+        {
             head = newNode;
-
         }
 
         // traverse till the end and append new node
-        else {
-            ListNode* tmp = head;
+        else
+        {
+            ListNode *tmp = head;
 
             // iterate to the index
-            while ( tmp->next ) {
+            while (tmp->next)
+            {
                 tmp = tmp->next;
             }
             tmp->next = newNode;
@@ -34,23 +37,27 @@ public:
         return head;
     }
 
-    ListNode* removeElements(ListNode* head, int val) {
-        
+    ListNode *removeElements(ListNode *head, int val)
+    {
+
         // return nullptr if list empty
-        if ( !head ) return nullptr;
+        if (!head)
+            return nullptr;
 
         /* 1. traverse the list
         2. create another list with non-matching values
         */
 
         ListNode *traverse, *head2;
-        
+
         head2 = nullptr;
         traverse = head;
 
-        while( traverse ) {
-            if ( traverse->val != val ) {
-                head2 = addAtTail( traverse->val, head2 );
+        while (traverse)
+        {
+            if (traverse->val != val)
+            {
+                head2 = addAtTail(traverse->val, head2);
             }
             traverse = traverse->next;
         }
