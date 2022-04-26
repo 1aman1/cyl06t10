e15b240df0919481d5d8c15c462167f52a2147ec
1aman1 <amangupta@outlook.com>
2022-04-26 16:00:23 +0530

    nodes connected or not

--- Graphs/AdjacencyList/path exists or not.cpp
+++ Graphs/AdjacencyList/path exists or not.cpp
@@ -4,81 +4,86 @@
 #include <iterator>
 
 /*
-implement graph,
+1-> implement graph using array,
 
-use a vector of lists
-
-then a function to check if path exists or not
+2-> a function to check if two nodes are connected
 */
 
 struct graph
 {
-    std::list<int> *adjacencyList;
-    graph(int v) : vertices(v)
-    {
-        adjacencyList = new std::list<int>[vertices];
-    }
-    int vertices;
-    void print();
-    void add_edge(int, int);
-    bool pathExists(int, int);
+	// graph utility
+	std::list<int>* adjacencyList;
+
+	graph(int v) : vertices(v)
+	{
+		adjacencyList = new std::list<int>[vertices];
+	}
+
+	int vertices;
+	void add_edge(int, int);
+
+	// graph aux
+	void print();
+
+	// path utility
+	bool pathExists(int, int);
 };
 
 void graph::print()
 {
-    for (int i = 1; i < 7; ++i)
-    {
-        std::cout << "vertex :" << i << " : ";
-        {
-            std::list<int>::iterator it;
-            for (it = adjacencyList[i].begin(); it != adjacencyList[i].end(); ++it)
-            {
-                std::cout << *it << ", ";
-            }
-            std::cout << std::endl;
-        }
-    }
+	for (int i = 1; i < 7; ++i)
+	{
+		std::cout << "vertex :" << i << " : ";
+		{
+			std::list<int>::iterator it;
+			for (it = adjacencyList[i].begin(); it != adjacencyList[i].end(); ++it)
+			{
+				std::cout << *it << ", ";
+			}
+			std::cout << std::endl;
+		}
+	}
 }
 
 void graph::add_edge(int u, int v)
 {
-    adjacencyList[u].push_back(v);
+	adjacencyList[u].push_back(v);
 }
 
 bool graph::pathExists(int u, int v)
 {
-    if (adjacencyList[u].empty())
-        return false;
-    for (std::list<int>::iterator it = adjacencyList[u].begin(); it != adjacencyList[u].end(); ++it)
-    {
-        if (*it == v)
-            return true;
-    }
+	if (adjacencyList[u].empty())
+		return false;
+	for (std::list<int>::iterator it = adjacencyList[u].begin(); it != adjacencyList[u].end(); ++it)
+	{
+		if (*it == v)
+			return true;
+	}
 
-    return false;
+	return false;
 }
 
 int main()
 {
-    graph go(7);
+	graph go(7);
 
-    go.add_edge(1, 2);
-    go.add_edge(1, 4);
+	go.add_edge(1, 2);
+	go.add_edge(1, 4);
 
-    go.add_edge(2, 5);
+	go.add_edge(2, 5);
 
-    go.add_edge(3, 5);
-    go.add_edge(3, 6);
+	go.add_edge(3, 5);
+	go.add_edge(3, 6);
 
-    go.add_edge(4, 2);
+	go.add_edge(4, 2);
 
-    go.add_edge(5, 4);
+	go.add_edge(5, 4);
 
-    go.add_edge(6, 6);
+	go.add_edge(6, 6);
 
-    go.print();
+	go.print();
 
-    std::cout << "path exists " << std::boolalpha << go.pathExists(6, 1) << std::endl;
+	std::cout << "path exists " << std::boolalpha << go.pathExists(6, 1) << std::endl;
 
-    return 0;
+	return 0;
 }
