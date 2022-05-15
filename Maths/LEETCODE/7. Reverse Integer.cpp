af85e038920fe0c6ef223e8730de6b091bb1171b
1aman1 <amangupta@outlook.com>
2022-05-15 12:36:36 +0530

    comments

--- Maths/LEETCODE/7. Reverse Integer.cpp
+++ Maths/LEETCODE/7. Reverse Integer.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include <string>
 #include <limits.h>
-#define DEBUG false
-
-/*
-
-Dividing MAX_VALUE by 10 lets you check the condition without overflowing
-
-INT_MAX is equal 2147483647. INT_MIN is equal  -2147483648.
-
-Last digits are 7 and 8. This is the reason why we also  check  rem > 7 and rem < -8 conditions
-
-*/
 
 class Solution
 {
 public:
-    int reverse(int num)
+    int reverse(int x)
     {
         int reversedNumber = 0;
 
-        while (num != 0)
-        {
-            int rem = num % 10;
-            num /= 10;
+        /*
+        INT_MAX is +2147483647
+        INT_MIN is -2147483648.
+        */
 
-            //                                                                     RANGE (0,7)
+        while (x != 0)
+        {
+            int rem = x % 10;
+            x /= 10;
+
+            //                                                                  && VALID RANGE (0,7)
             if (reversedNumber > INT_MAX / 10 || reversedNumber == INT_MAX / 10 && rem > 7)
             {
                 return 0;
             }
-            //                                                                     RANGE (-8,0)
+
+            //                                                                  && VALID RANGE (-8,0)
             if (reversedNumber < INT_MIN / 10 || reversedNumber == INT_MIN / 10 && rem < -8)
             {
                 return 0;
