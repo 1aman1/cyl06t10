3f5ddea502b396144c86f7af0d9dd4eee9bedf3d
1aman1 <amangupta@outlook.com>
2022-08-29 10:38:26 +0530

    used constexpr & updated test tree

--- Trees/Book Problems/CtCI-4.4-Check Balanced.cpp
+++ Trees/Book Problems/CtCI-4.4-Check Balanced.cpp
@@ -2,24 +2,23 @@
 #include <algorithm>
 #include <math.h>
 
-#define ERR_VALUE -99999
+constexpr int ERR_VAL = -99999;
 
 /*
 1. find height of subtrees
 
-2. if they differ by > 1
-not a BST,
-
+2. if they differ by > 1, then it is not a Balanced Tree
 */
 
-struct tree_node
+struct node
 {
-    int node_data;
-    tree_node *left;
-    tree_node *right;
-    tree_node(int newdata) : node_data(newdata),
-                             left(nullptr),
-                             right(nullptr) {}
+    int datum;
+    node *left;
+    node *right;
+    
+    node(int newdata) : datum(newdata),
+                        left(nullptr),
+                        right(nullptr) {}
 };
 
 /*
@@ -29,12 +28,12 @@ uses maxDepth, isBalanced
 O(N * log(N))
 */
 
-int maxDepth(tree_node *root)
+int maxDepth(node *root)
 {
     return root == NULL ? 0 : std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
 }
 
-bool isBalanced(tree_node *root)
+bool isBalanced(node *root)
 {
     if (!root)
         return true;
@@ -54,47 +53,40 @@ O(N)
 
 */
 
-int checkHeight(tree_node *root)
+int checkHeight(node *root)
 {
     if (root == nullptr)
         return -1;
 
     int leftHeight = checkHeight(root->left);
-    if (leftHeight == ERR_VALUE)
-        return ERR_VALUE;
+    if (leftHeight == ERR_VAL)
+        return ERR_VAL;
 
     int rightHeight = checkHeight(root->right);
-    if (rightHeight == ERR_VALUE)
-        return ERR_VALUE;
+    if (rightHeight == ERR_VAL)
+        return ERR_VAL;
 
-    int heightDifference = leftHeight - rightHeight;
-    if (std::abs(heightDifference) > 1)
-        return ERR_VALUE;
+    if (std::abs(leftHeight - rightHeight) > 1)
+        return ERR_VAL;
     else
-    {
         return std::max(leftHeight, rightHeight) + 1;
-    }
 }
 
-bool isBalanced_2nd(tree_node *root)
+bool isBalanced_2nd(node *root)
 {
-    return (checkHeight(root) != ERR_VALUE);
+    return (checkHeight(root) != ERR_VAL);
 }
 
 int main()
 {
-    tree_node *root = new tree_node(4);
-    root->left = new tree_node(4);
-    // root->left->right = new tree_node(4); // unbalancing node
-    root->left->left = new tree_node(4);
-    root->left->left->right = new tree_node(4);
+    node *root = new node(1);
+    root->left = new node(1);
 
-    root->right = new tree_node(1);
-    root->right->left = new tree_node(1);
-    root->right->right = new tree_node(1);
+    root->right = new node(1);
+    // root->left->right = new node(1);
 
-    // std::cout << "Is tree balanced ?" << std::boolalpha << isBalanced(root) << std::endl;
-    std::cout << "Is tree balanced ?" << std::boolalpha << isBalanced_2nd(root) << std::endl;
+    // std::cout << "Is tree balanced ? " << std::boolalpha << isBalanced(root) << std::endl;
+    std::cout << "Is tree balanced ? " << std::boolalpha << isBalanced_2nd(root) << std::endl;
 
     return 0;
 }
