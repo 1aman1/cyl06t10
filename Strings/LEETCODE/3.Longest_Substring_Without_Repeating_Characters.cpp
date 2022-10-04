7ba90569202d6b117b3d41af19280118dad7c376
1aman1 <amangupta@outlook.com>
2022-10-04 14:44:19 +0530

    +

--- Strings/LEETCODE/3.Longest_Substring_Without_Repeating_Characters.cpp
+++ Strings/LEETCODE/3.Longest_Substring_Without_Repeating_Characters.cpp
@@ -3,19 +3,14 @@ class Solution
 public:
     int lengthOfLongestSubstring(string s)
     {
-        int longest = 0;
-        int length = s.length() - 1;
-
         map<char, int> lookup;
 
         int lhs = 0;
-        for (int rhs = 0; rhs <= length; ++rhs)
+        int longest = 0;
+        for (int rhs = 0; rhs < s.length(); ++rhs)
         {
-            // s[rhs];
-
             if (lookup.find(s[rhs]) != lookup.end())
             {
-                // update left
                 lhs = std::max(lhs,
                                lookup[s[rhs]] + 1);
             }
