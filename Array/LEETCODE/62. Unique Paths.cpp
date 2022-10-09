ff70088d6302778b428c90e0fa3a615c1f3496cd
1aman1 <amangupta@outlook.com>
2022-10-09 22:21:19 +0530

    upd

--- Array/LEETCODE/62. Unique Paths.cpp
+++ Array/LEETCODE/62. Unique Paths.cpp
@@ -1,45 +1,14 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution
 {
 public:
-    int uniquePaths(int m, int n)
-    {
-       int* dp = new int[n];
-        
-	dp[0] = 1;
+	int uniquePaths(int m, int n)
+	{
+		std::vector<std::vector<int>> cache(row, std::vector<int>(col, 1));
 
-	for (int i = 0; i < m; i++) {
-		for (int j = 1; j < n; j++) {
-			dp[j] += dp[j - 1];
-		}
-        
-	return dp[n-1];
-    }
+		for (int r = 1; r < row; ++r)
+			for (int c = 1; c < col; ++c)
+				cache[r][c] = cache[r - 1][c] + cache[r][c - 1];
+
+		return cache[row - 1][col - 1];
+	}
 };
