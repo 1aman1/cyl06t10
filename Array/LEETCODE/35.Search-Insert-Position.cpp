0adc4601f85bd6319185684f61591bd9a4990f62
1aman1 <amangupta@outlook.com>
2022-10-14 08:48:31 +0530

    revisiting

--- Array/LEETCODE/35.Search-Insert-Position.cpp
+++ Array/LEETCODE/35.Search-Insert-Position.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution
 {
 public:
-    int searchInsert(std::vector<int> &nums, int target)
+    int searchInsert(vector<int> &nums, int target)
     {
         int left = 0, right = nums.size() - 1;
         int track_greater_number = right;
@@ -36,12 +38,38 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            const int mid = (right - left) / 2 + left;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (target > nums[mid])
+                left = mid + 1;
+            else
+                right = mid; // inclusively
+        }
+
+        return left;
+    }
+};
+
 int main()
 {
-    std::vector<int> nums = {1, 3, 5, 6};
+    vector<int> nums = {1, 3, 5, 6};
     Solution obj;
-    std::cout << obj.searchInsert(nums, 5);
-    std::cout << obj.searchInsert(nums, 2);
-    std::cout << obj.searchInsert(nums, 7);
+
+    cout << obj.searchInsert(nums, 5) << "\n";
+    cout << obj.searchInsert(nums, 2) << "\n";
+    cout << obj.searchInsert(nums, 7) << "\n";
     return 0;
 }
