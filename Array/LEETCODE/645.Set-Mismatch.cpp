4b43812e10bc3a700fd88c5d6f3de1c5317032df
1aman1 <amangupta@outlook.com>
2022-10-23 15:37:12 +0530

    +

--- Array/LEETCODE/645.Set-Mismatch.cpp
+++ Array/LEETCODE/645.Set-Mismatch.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 using namespace std;
+
 class Solution
 {
 public:
@@ -20,7 +21,7 @@ public:
         for (int i = 0; i < nums.size(); ++i)
             if (nums[i] > 0)
                 return {duplicate, i + 1};
-                
+
         return {};
     }
 };
