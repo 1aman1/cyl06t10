27096ed25bb6a0870e0969a630335f2424c619f2
1aman1 <amangupta@outlook.com>
2022-10-25 13:13:40 +0530

    reduced space usage

--- Strings/LEETCODE/1662.Check-If-Two-String-Arrays-are-Equivalent.cpp
+++ Strings/LEETCODE/1662.Check-If-Two-String-Arrays-are-Equivalent.cpp
@@ -9,26 +9,29 @@ class Solution
 public:
     bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2)
     {
-        list<char> W1, W2;
+        list<char> word1Buffer;
 
         for (int i = 0; i < word1.size(); ++i)
         {
-            string tmp = word1[i];
-            for (int l = 0; l < tmp.size(); ++l)
+            string currWord = word1[i];
+            for (int l = 0; l < currWord.size(); ++l)
             {
-                W1.push_back(tmp[l]);
-            }
-        }
-        for (int i = 0; i < word2.size(); ++i)
-        {
-            string tmp = word2[i];
-            for (int l = 0; l < tmp.size(); ++l)
-            {
-                W2.push_back(tmp[l]);
+                word1Buffer.push_back(currWord[l]);
             }
         }
 
-        return W1 == W2;
+        for (int i = word2.size() - 1; i >= 0; --i)
+        {
+            string currWord = word2[i];
+            for (int l = currWord.size() - 1; l >= 0; --l)
+            {
+                if (currWord[l] != word1Buffer.back())
+                    return false;
+                word1Buffer.pop_back();
+            }
+        }
+
+        return true;
     }
 };
 
@@ -36,8 +39,10 @@ int main()
 {
     Solution obj;
 
-    vector<string> word1 = {"ab", "c"};
-    vector<string> word2 = {"a", "bc"};
+    // vector<string> word1 = {"ab", "c"};
+    // vector<string> word2 = {"a", "bc"};
+    vector<string> word1 = {"abc", "d", "defg"};
+    vector<string> word2 = {"abcddefg"};
 
     cout << boolalpha
          << obj.arrayStringsAreEqual(word1, word2)
