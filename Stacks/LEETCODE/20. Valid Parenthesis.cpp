e1daeae8a2cc411b0d79e7f50b5bace0dd166be5
1aman1 <amangupta@outlook.com>
2023-06-12 11:51:51 +0530

    improved readability

--- Stacks/LEETCODE/20. Valid Parenthesis.cpp
+++ Stacks/LEETCODE/20. Valid Parenthesis.cpp
@@ -2,49 +2,29 @@ class Solution
 {
 public:
     bool isValid(string s)
-
     {
         std::stack<char> myStack;
-        char currChar;
 
-        for (long unsigned int i = 0; i < s.length(); ++i)
+        for (const char current : s)
         {
-            // store to validate further
-            currChar = s[i];
-
-            // validate
-            if (currChar == '(' || currChar == ')' || currChar == '[' || currChar == ']' || currChar == '{' || currChar == '}')
+            if (current == '(' || current == '[' || current == '{')
             {
-                // if opening bracket
-                if (currChar == '(' || currChar == '[' || currChar == '{')
-                {
-                    myStack.push(currChar);
-                    continue;
-                }
-
-                // when closing bracket appears first
-                if (myStack.size() == 0)
-                {
-                    return false;
-                }
-
-                // if closing bracket, then validate it with most recent bracket
-
-                char top = myStack.top();
-
-                // match the closing brackets properly
-                if ((currChar == ')' && top == '(') || (currChar == '}' && top == '{') || (currChar == ']' && top == '['))
-                    myStack.pop();
-                else
-                    break;
+                myStack.push(current);
+                continue;
             }
 
-            // skip validation when the current charater is a bracket
-            continue;
+            if (myStack.size() == 0) // when closing bracket appears first
+                return false;
+
+            char top = myStack.top();
+
+            // match the closing brackets properly
+            if ((current == ')' && top == '(') || (current == '}' && top == '{') || (current == ']' && top == '['))
+                myStack.pop();
+            else
+                break;
         }
-        if (myStack.size() == 0)
-            return true;
-        else
-            return false;
+
+        return myStack.empty();
     }
 };
